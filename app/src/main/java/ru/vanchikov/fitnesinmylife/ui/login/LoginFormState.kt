566b1af839ae3af9333bbc916d403ae6a87d0a1a
OneChi <alexbvbv@yandex.ru>
2019-08-01 13:21:33 +0300

    Initial Commit

---
+++ app/src/main/java/ru/vanchikov/fitnesinmylife/ui/login/LoginFormState.kt
@@ -0,0 +1,10 @@
+package ru.vanchikov.fitnesinmylife.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(
+    val usernameError: Int? = null,
+    val passwordError: Int? = null,
+    val isDataValid: Boolean = false
+)
