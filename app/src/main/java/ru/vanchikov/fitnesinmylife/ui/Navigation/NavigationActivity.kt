2367fa7717bb13ae58aad0daafee8e3e837f827e
OneChi <alexbvbv@yandex.ru>
2019-08-07 15:04:44 +0300

    Side menu with nav_graph connected

--- app/src/main/java/ru/vanchikov/fitnesinmylife/ui/Navigation/NavigationActivity.kt
+++ app/src/main/java/ru/vanchikov/fitnesinmylife/ui/Navigation/NavigationActivity.kt
@@ -1,38 +1,30 @@
 package ru.vanchikov.fitnesinmylife.ui.Navigation
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.core.view.GravityCompat
-import androidx.appcompat.app.ActionBarDrawerToggle
+import android.view.Menu
 import android.view.MenuItem
-import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.material.navigation.NavigationView
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import android.view.Menu
-import android.view.View
-import android.widget.TextView
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import kotlinx.android.synthetic.main.activity_login.*
-import ru.vanchikov.fitnesinmylife.R
-import ru.vanchikov.fitnesinmylife.data.model.LoggedInUser
-import ru.vanchikov.fitnesinmylife.ui.login.LoginViewModel
-import ru.vanchikov.fitnesinmylife.ui.login.LoginViewModelFactory
-import android.app.Dialog
-import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
-import ru.vanchikov.fitnesinmylife.data.UserAccount
 import androidx.navigation.ui.NavigationUI
-
-import androidx.navigation.NavController
 import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_navigation.*
+import kotlinx.android.synthetic.main.app_bar_navigation.*
+import ru.vanchikov.fitnesinmylife.R
+import ru.vanchikov.fitnesinmylife.data.UserAccount
+import ru.vanchikov.fitnesinmylife.data.model.LoggedInUser
 
 
 class NavigationActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
@@ -44,25 +36,36 @@ class NavigationActivity : AppCompatActivity(), NavigationView.OnNavigationItemS
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(ru.vanchikov.fitnesinmylife.R.layout.activity_navigation)
-        val toolbar: Toolbar = findViewById(ru.vanchikov.fitnesinmylife.R.id.toolbar)
-        val navController = Navigation.findNavController(this, ru.vanchikov.fitnesinmylife.R.id.nav_host_fragment)
+        setContentView(R.layout.activity_navigation)
 
-        NavigationUI.setupWithNavController(toolbar, navController)
+        initMenu()
+
+    }
+
+    private lateinit var navController: NavController
+
+    private fun initMenu() {
+
+        val host = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        val navController = host.navController
+
+        // обеспечивает поддержку логинап и почты в оглавлении бокового меню NavDrawer
         setSupportActionBar(toolbar)
 
-
-        val fab: FloatingActionButton = findViewById(ru.vanchikov.fitnesinmylife.R.id.fab)
+        val fab: FloatingActionButton = findViewById(R.id.fab)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
-        val drawerLayout: DrawerLayout = findViewById(ru.vanchikov.fitnesinmylife.R.id.drawer_layout)
-        val appBarConfiguration = AppBarConfiguration.Builder(navController.graph).setDrawerLayout(drawerLayout).build()
 
-        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
 
-        val navView: NavigationView = findViewById(ru.vanchikov.fitnesinmylife.R.id.nav_view)
+        val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout = drawer_layout)
+
+
+        val navView: NavigationView = findViewById(R.id.nav_view)
         val toggle = ActionBarDrawerToggle(
             this, drawerLayout, toolbar, ru.vanchikov.fitnesinmylife.R.string.navigation_drawer_open, ru.vanchikov.fitnesinmylife.R.string.navigation_drawer_close
         )
@@ -70,9 +73,9 @@ class NavigationActivity : AppCompatActivity(), NavigationView.OnNavigationItemS
         toggle.syncState()
 
         navView.setNavigationItemSelectedListener(this)
+        navView?.setupWithNavController(navController)
 
-
-
+        toolbar.setupWithNavController(navController,appBarConfiguration)
 
     }
 
@@ -114,14 +117,14 @@ class NavigationActivity : AppCompatActivity(), NavigationView.OnNavigationItemS
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            ru.vanchikov.fitnesinmylife.R.id.nav_home -> {
-                Navigation.findNavController(this,R.id.nav_host_fragment).navigate(R.id.userPage)
+                ru.vanchikov.fitnesinmylife.R.id.userPage -> {
+                //Navigation.findNavController(this,R.id.nav_host_fragment).navigate(R.id.userPage)
             }
             ru.vanchikov.fitnesinmylife.R.id.nav_ways -> {
                 //Navigation.findNavController(this,R.id.nav_host_fragment).navigate(R.id.mapPage)
             }
             ru.vanchikov.fitnesinmylife.R.id.mapPage -> {
-                Navigation.findNavController(this,R.id.nav_host_fragment).navigate(R.id.mapPage)
+                //Navigation.findNavController(this,R.id.nav_host_fragment).navigate(R.id.mapPage)
             }
             ru.vanchikov.fitnesinmylife.R.id.nav_share -> {
 
@@ -135,3 +138,6 @@ class NavigationActivity : AppCompatActivity(), NavigationView.OnNavigationItemS
         return true
     }
 }
+
+
+
